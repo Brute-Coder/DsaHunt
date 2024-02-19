da3d5ed728ebb5ccef143c1b8123c90b6588bb02
Soumya Sekhar Mahanti <soumyasekharmahanti@gmail.com>
2024-02-19 21:10:34 +0530

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/header/Navbar";
+import { Analytics } from "@vercel/analytics/react";
 export default function App() {
   return (
     <div className="bg-gradient-to-tl from-slate-800 to-slate-700 h-screen w-screen text-white">
+      <Analytics />
       <Navbar />
       <Outlet />
     </div>
